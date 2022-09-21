eac1284d6f71fc0c9c232c324eab1c1ab29f5adc
jlblynn <jlblynn90@gmail.com>
2022-09-21 18:38:31 +0800

    debugging client submodule

---
+++ server/sampleData.js
@@ -0,0 +1,76 @@
+// Projects
+
+const projects = [
+  {
+    id: '1',
+    clientId: '1',
+    name: 'SurfApp',
+    description: 'Choosing a break to surf just got a whole lot easier.',
+    status: 'In Progress'
+  },
+  {
+    id: '2',
+    clientId: '2',
+    name: 'Web Brains',
+    description: 'Web Brains is a creative agency that specialises in Web, Logo and SEO.',
+    status: 'In Progress'
+  },
+  {
+    id: '3',
+    clientId: '3',
+    name: 'Coffee Find',
+    description: 'Be paid to pick up someone elses coffee order.',
+    status: 'Not Started'
+  },
+  {
+    id: '4',
+    clientId: '4',
+    name: 'NTF Mega Wall',
+    description: 'An NFT version of The Homepage.',
+    status: 'Not Started'
+  },
+  {
+    id: '5',
+    clientId: '5',
+    name: 'E-racing Pro',
+    description: 'Get the advantage of data to improve your racecraft.',
+    status: 'Not Started'
+  }
+];
+
+// Clients
+
+const clients = [
+  {
+    id: '1',
+    name: 'Xavier Hendricks',
+    email: 'surfapp@gmail.com',
+    phone: '111111111'
+  },
+  {
+    id: '2',
+    name: 'Josh Linn',
+    email: 'contact@webbrains.io',
+    phone: '222222222'
+  },
+  {
+    id: '3',
+    name: 'Kate Louise',
+    email: 'coffeefind@gmail.com',
+    phone: '333333333'
+  },
+  {
+    id: '4',
+    name: 'Xi Ming',
+    email: 'nftwall@crypto.xyz',
+    phone: '444444444'
+  },
+  {
+    id: '5',
+    name: 'Kevin Broadhurst',
+    email: 'eracing@gmail.com',
+    phone: '555555555'
+  }
+];
+
+module.exports = { projects, clients };
